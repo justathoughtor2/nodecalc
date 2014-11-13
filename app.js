e1cfb7fd5450dc4e43ffc187fb25bd678d19042d
justathoughtor2 <thoughthaven@gmail.com>
2014-11-13 00:11:15 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,60 +1,48 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+/**
+ * Module dependencies.
+ */
 
-var app = express();
+var express = require('express')
+  , routes = require('./routes');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+var app = module.exports = express.createServer();
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// Configuration
 
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+app.use(express.compiler({ src : __dirname + '/public', enable: ['less']}));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
 });
 
-// error handlers
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// Compatible
+
+// Now less files with @import 'whatever.less' will work(https://github.com/senchalabs/connect/pull/174)
+var TWITTER_BOOTSTRAP_PATH = './vendor/twitter/bootstrap/less';
+express.compiler.compilers.less.compile = function(str, fn){
+  try {
+    var less = require('less');var parser = new less.Parser({paths: [TWITTER_BOOTSTRAP_PATH]});
+    parser.parse(str, function(err, root){fn(err, root.toCSS());});
+  } catch (err) {fn(err);}
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+// Routes
+
+app.get('/', routes.index);
+
+app.listen(3000, function(){
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
-
-
-module.exports = app;
